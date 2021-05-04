04a397af76f53e2b24813f7e2e18048e474551d5
Gonzalo Gluzman <gonzalo.gluzman@stensul.com>
2021-05-04 20:41:33 -0300

    Final del segundo video

--- src/json-parser.ts
+++ src/json-parser.ts
@@ -27,9 +27,6 @@ const TuplaS: (s: string) => <T> (x: T) => TuplaS<T> =
         [s, x]
 ;
 
-const mapArray: <A, B> (fn: (x: A) => B) => (arr: A[]) => B[] = fn => arr =>
-    arr.map(fn)
-;
 const saraza = null as any;
 
 const mapTuplaS: <A, B> (fn: (x: A) => B) => (t: TuplaS<A>) => TuplaS<B> =
@@ -79,7 +76,7 @@ const isResult = <T> (mf: MayFail <T>): mf is Result <T> =>
     mf.tag === 'success'
 ;
 
-const mapMayFail: <A, B> (fn: (x: A) => B) => (t: MayFail<A>) => MayFail<B> =
+const mapMayFail: <A, B> (fn: (x: A) => B) => (mf: MayFail<A>) => MayFail<B> =
     fn => mf =>
         isResult (mf) ?
             Result (fn (mf.value)) :
@@ -92,9 +89,14 @@ export const parserS: <S extends string> (expected: S) => Parser<S> = expectedSt
     const firstChunk = input.slice (0, expectedLength);
     const remaining = input.slice (expectedLength);
 
+    const errorMessage = input.length < expectedLength ?
+        'Unexpected end of input' :
+        `Unexpected "${input}"`
+    ;
+
     return firstChunk === expectedString ?
         Result (TuplaS (remaining) (expectedString)) :
-        Failure (`Unexpected "${input}"`)
+        Failure (errorMessage)
     ;
 };
 
@@ -146,3 +148,41 @@ const anyP: {
 ;
 
 export const jsonParser: Parser<JsonValue> = anyP ([nullParser, booleanParser]);
+
+const flatParser: <T> (outsideParser: Parser<Parser<T>>) => Parser<T> =
+    outsideParser =>
+        input => {
+            const outsideResult = outsideParser (input);
+
+            if (isResult (outsideResult)) {
+
+                const [ outsideRemaining, insideParser ] = outsideResult.value;
+
+                return insideParser (outsideRemaining);
+            }
+            return outsideResult;
+        }
+;
+
+const applyParser = <A, B> (parserFn: Parser<(x: A) => B>) => (parserX: Parser<A>): Parser<B> =>
+    flatParser (mapParser ((fn: (x: A) => B) => mapParser (fn) (parserX)) (parserFn))
+;
+
+const liftA2Parser: <A, B, C> (fn: (x: A) => (y: B) => C) => (p1: Parser<A>) => (p2: Parser<B>) => Parser<C> =
+    fn => p1 => p2 =>
+        applyParser (mapParser (fn) (p1)) (p2);
+;
+
+const concatParsers = liftA2Parser;
+
+const concatStringParsers: (p1: Parser<string>) => (p2: Parser<string>) => Parser<string> =
+    p1 => p2 =>
+        concatParsers ((x: string) => (y: string) => x + y) (p1) (p2)
+;
+
+export const Parser: <T> (x: T) => Parser<T> =
+    x =>
+        input =>
+            Result (TuplaS (input) (x))
+;
+
